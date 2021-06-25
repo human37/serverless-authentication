a70da590c41d650cec014e3e8eeeb6ab00d596f8
Hunter D. Nielson <hnielson@CIT-SCC112-1123.cs.dixie.edu>
2021-06-25 10:37:14 -0600

    add model.js

---
+++ functions/utils/model.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const hashSchema = mongoose.Schema({
+    userID: String,
+    password: String
+});
+
+const stopIt = mongoose.model("stopIt", hashSchema);
+
+module.exports = {
+    stopIt
+};

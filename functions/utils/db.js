0e274fab74bf6cef56e730867eb7301c0fce3de8
dnielson44 <d00290186@dmail.dixie.edu>
2021-06-25 10:23:34 -0600

    database connection

---
+++ functions/utils/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+
+function connect(){
+    let connectionString = `mongodb+srv://DawsonNielson:Dawsonp44@cluster0.08nxx.mongodb.net/serverlessAuthentication?retryWrites=true&w=majority`;
+    console.log("connect to db....");
+    mongoose.connect(connectionString,{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+}
+
+function onConnect(callback){
+    mongoose.connection.once("open",callback);
+}
+
+module.exports = {
+    connect,
+    onConnect,
+};
